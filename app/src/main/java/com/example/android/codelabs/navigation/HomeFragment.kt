9327eef1e6cc34aca4b3d237b607cc33b477aa4e
Robert Gitei <robert.gitei@yopeso.com>
2022-08-19 14:32:11 +0300

    Using the Navigation Graph to Navigate

--- app/src/main/java/com/example/android/codelabs/navigation/HomeFragment.kt
+++ app/src/main/java/com/example/android/codelabs/navigation/HomeFragment.kt
@@ -18,7 +18,9 @@ package com.example.android.codelabs.navigation
 
 import android.os.Bundle
 import android.view.*
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 
 /**
  * Fragment used to show how to navigate to another destination
@@ -42,6 +44,11 @@ class HomeFragment : Fragment() {
 //            findNavController().navigate(R.id.flow_step_one_dest, null)
 //        }
         //TODO END STEP 5
+        val button = view.findViewById<Button>(R.id.navigate_destination_button)
+        button?.setOnClickListener {
+            findNavController().navigate(R.id.flow_step_one_dest, null)
+        }
+
 
         //TODO STEP 6 - Set NavOptions
 //        val options = navOptions {
